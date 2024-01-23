f239649f91efaf7e8b166bd1a5ff2bcd892a4bb8
CoderSid063 <sudhansupanda063@gmail.com>
2024-01-23 12:01:39 +0530

    feat: complete Home and About sections

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,25 @@
+import Image from "next/image";
+import styles from "./home.module.css";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Home Page
-    </main>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thought Ideas</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis aliquam
+          non unde .
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg} />
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
+      </div>
+    </div>
   );
 }
